669a5ea67bd6be4779198a24d690b7c12c046f58
Êxodo Gurgel <exodowellis@gmail.com>
2023-02-02 11:02:26 -0300

    Challenge Finalized

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -6,11 +6,17 @@ import {
 } from 'react';
 
 import { useField } from '@unform/core';
-
 import { Container } from './styles';
+import { InputHTMLAttributes } from 'react';
+import { IconType } from 'react-icons';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string
+  icon?: IconType;
+}
+
+export function Input ({ name, icon: Icon, ...rest }: InputProps){
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -49,5 +55,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
